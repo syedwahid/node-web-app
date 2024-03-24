60016fc6e552cbf419293294417b74fe43978d73
syedwahid <syedwahid@gmail.com>
2024-03-24 09:02:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('New Hello World!')
 })
 //Launch listening server on port 8888
 app.listen(8888, function () {
